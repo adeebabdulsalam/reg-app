4db976bc6d2b4449e5c134c2067c7c7188747eb6
Adeeb Abdul Salam <adeebnabdulsalam@gmail.com>
2017-08-31 00:03:39 +0530

    Add files via upload

--- search.js
+++ search.js
@@ -1,11 +1,40 @@
 $(document).ready(function(){
+ const eventnames=[];
+
+ function display_bio(name,college,mobile){
+     $('#result-bio').html("Name:"+name+"<br><br>College:"+college+"<br><br>Mobile:"+mobile);
+ }
  
- const root=firebase.database().ref('tathva17/Participants');
+ function display_event(eventso){
+    var newevents=$.map(array, function(value){
+        return('<td>'+value+'</td>');
+    });
+     $('#result-event').html(newevents.join(""));
+ }
  
 $('#search-button').click(function(){
-    const search_name=$('#search').val();
-    root.orderByChild('Name').equalTo(search_name).once('value',function(snap){
-        console.log(snap.val().Name);
+    const root=firebase.database().ref('/tathva17/users');
+    const search_email=$('#search').val();
+  
+    root.orderByChild('email').equalTo(search_email).once('child_added',function(snap){
+           var name=snap.child('bio/name').val();
+           var mobile=snap.child('bio/mobile').val();
+           var college=snap.child('bio/college').val();
+           
+           snap.child('events').forEach(function(eid){
+                 console.log("event id:"+eid.key+",status:"+eid.val());
+                var eventcontent=firebase.database().ref('/tathva17/events/'+eid.key);
+                eventcontent.child('name').once('value',function(name){
+                   eventnames.push(name.val());
+                  //display_event(ename);  
+                  console.log("eventname:"+name.val());  
+                });     
+              });
+             console.log(eventnames); 
+            display_event(eventnames);  
+           display_bio(name,college,mobile);  
+    } ,function(error){
+        console.log("Wrong entry"+error);
     });
     
     
