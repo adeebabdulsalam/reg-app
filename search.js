a6fca7f68e105964f239351cc5ff746a8a156284
Adeeb Abdul Salam <adeebnabdulsalam@gmail.com>
2017-09-02 20:53:31 +0530

    Add files via upload

--- search.js
+++ search.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
- const eventnames=[];
 
+ $('#master').click(function(){
+     $('.checkall').prop('checked', $(this).prop('checked'));
+ });
+ 
  function display_bio(name,college,mobile){
-     $('#result-bio').html("Name:"+name+"<br><br>College:"+college+"<br><br>Mobile:"+mobile);
+     $('#result-clg').html(college);
+     $('#result-num').html(mobile);
+     $('#result-name').html(name);
+     $('#temp-name').hide();
  }
  
- function display_event(eventso){
-    var newevents=$.map(array, function(value){
-        return('<td>'+value+'</td>');
-    });
-     $('#result-event').html(newevents.join(""));
- }
+
  
 $('#search-button').click(function(){
+    $('#result-bio').html("");
+    $('#result-name').html("");
+    $('#result-event').html("");
     const root=firebase.database().ref('/tathva17/users');
     const search_email=$('#search').val();
   
@@ -24,20 +28,23 @@ $('#search-button').click(function(){
            snap.child('events').forEach(function(eid){
                  console.log("event id:"+eid.key+",status:"+eid.val());
                 var eventcontent=firebase.database().ref('/tathva17/events/'+eid.key);
-                eventcontent.child('name').once('value',function(name){
-                   eventnames.push(name.val());
-                  //display_event(ename);  
-                  console.log("eventname:"+name.val());  
+                eventcontent.once('value',function(snapevent){
+                   var venue=snapevent.child('venue').val();
+                   var time=snapevent.child('time').val();
+                   var amount=snapevent.child('amount').val();
+                   var name=snapevent.child('name').val();
+                   console.log("Venue:"+venue+"name:"+name);
+                  
+                 // $('#result-event').append("<tr><td><div class='squaredOne'><input type='checkbox' class='checkall' value='None' id='"+eid.key+"' name='check' checked /><label for='"+eid.key+"'></label></div></td><td class='mdl-data-table__cell--non-numeric'>"+name+"</td><td class='mdl-data-table__cell--non-numeric'>"+venue+"</td><td class='mdl-data-table__cell--non-numeric'>"+time+"</td><td>"+amount+"</td></tr>");
+                $('#result-event').append("<tr><td><div class='squaredOne'><input type='checkbox' class='checkall' value='None' id='"+eid.key+"' name='check' checked /><label for='"+eid.key+"'></label></div></td><td>"+name+"</td><td>"+venue+"</td><td>"+time+"</td><td>"+amount+"</td></tr>");
                 });     
               });
-             console.log(eventnames); 
-            display_event(eventnames);  
+             //console.log(eventnames);  
            display_bio(name,college,mobile);  
     } ,function(error){
         console.log("Wrong entry"+error);
     });
-    
-    
+  
 });
 
 });
